126db4a2ada662a6a9da7538ede048ab723b58ab
Randi <rahandi@kcv.if.its.ac.id>
2019-04-15 19:24:58 +0700

    +update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,7 +79,125 @@ class HomeController extends Controller
 
     public function update(Request $request)
     {
-        $data = pendaftar::find();
+        $id = $request->id;
+        $data_utama = pendaftar::find($id);
+        $data_alamat_asal = alamat::find($data_utama->alamat_asal_id);
+        $data_alamat_surabaya = alamat::find($data_utama->alamat_surabaya_id);
+        $data_status_saat_mendaftar = statusSaatMendaftar::find($data_utama->status_saat_mendaftar_id);
+        $data_sumber_informasi = sumberInformasi::find($data_utama->sumber_informasi_id);
+        $pendidikan_id = unserialize($data_utama->pendidikan_id);
+
+        //bagian status saat mendaftar
+        $data_status_saat_mendaftar->lulus_sma = (isset($request->lulus_sma)) ? 1 : 0;
+        $data_status_saat_mendaftar->mahasiswa = (isset($request->mahasiswa)) ? 1 : 0;
+        $data_status_saat_mendaftar->bekerja = (isset($request->bekerja)) ? 1 : 0;
+        $data_status_saat_mendaftar->save();
+
+        //bagian sumber informasi
+        $data_sumber_informasi->koran = (isset($request->koran)) ? 1 : 0;
+        $data_sumber_informasi->spanduk = (isset($request->spanduk)) ? 1 : 0;
+        $data_sumber_informasi->brosur = (isset($request->brosur)) ? 1 : 0;
+        $data_sumber_informasi->teman_saudara = (isset($request->teman_saudara)) ? 1 : 0;
+        $data_sumber_informasi->pameran = (isset($request->pameran)) ? 1 : 0;
+        $data_sumber_informasi->lainnya = (isset($request->lainnya)) ? 1 : 0;
+        $data_sumber_informasi->save();
+
+        //bagian data pribadi
+        $data_utama->nama = $request->nama;
+        $data_utama->nama_gelar = $request->nama_gelar;
+        $data_utama->tempat_lahir = $request->tempat_lahir;
+        $data_utama->tanggal_lahir = $request->tanggal_lahir;
+        $data_utama->jenis_kelamin = $request->jenis_kelamin;
+        $data_utama->agama = $request->agama;
+        $data_utama->status_perkawinan = $request->status_perkawinan;
+        $data_utama->nomor_handphone = $request->nomor_handphone;
+
+        //bagian alamat
+        ///alamat asal
+        $data_alamat_asal->jalan = $request->asal_jalan;
+        $data_alamat_asal->kelurahan = $request->asal_kelurahan;
+        $data_alamat_asal->kecamatan = $request->asal_kecamatan;
+        $data_alamat_asal->kabupaten = $request->asal_kabupaten;
+        $data_alamat_asal->kode_pos = $request->asal_kode_pos;
+        $data_alamat_asal->telepon = $request->asal_telepon;
+        $data_alamat_asal->save();
+        ///alamat surabaya
+        $data_alamat_surabaya->jalan = $request->surabaya_jalan;
+        $data_alamat_surabaya->kelurahan = $request->surabaya_kelurahan;
+        $data_alamat_surabaya->kecamatan = $request->surabaya_kecamatan;
+        $data_alamat_surabaya->kabupaten = $request->surabaya_kabupaten;
+        $data_alamat_surabaya->kode_pos = $request->surabaya_kode_pos;
+        $data_alamat_surabaya->telepon = $request->surabaya_telepon;
+        $data_alamat_surabaya->save();
+
+        if (isset($request->sd_institusi, $request->sd_bidang_studi, $request->sd_tahun_masuk, $request->sd_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->sd);
+            $table_pendidikan->jenjang_pendidikan = 'sd';
+            $table_pendidikan->institusi = $request->sd_institusi;
+            $table_pendidikan->bidang_studi = $request->sd_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->sd_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->sd_tahun_lulus;
+            $table_pendidikan->save();
+        }
+
+        if (isset($request->sltp_institusi, $request->sltp_bidang_studi, $request->sltp_tahun_masuk, $request->sltp_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->sltp);
+            $table_pendidikan->jenjang_pendidikan = 'sltp';
+            $table_pendidikan->institusi = $request->sltp_institusi;
+            $table_pendidikan->bidang_studi = $request->sltp_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->sltp_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->sltp_tahun_lulus;
+            $table_pendidikan->save();
+        }
+
+        if (isset($request->slta_institusi, $request->slta_bidang_studi, $request->slta_tahun_masuk, $request->slta_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->slta);            
+            $table_pendidikan->jenjang_pendidikan = 'slta';
+            $table_pendidikan->institusi = $request->slta_institusi;
+            $table_pendidikan->bidang_studi = $request->slta_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->slta_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->slta_tahun_lulus;
+            $table_pendidikan->save();
+        }
+
+        if (isset($request->diploma_institusi, $request->diploma_bidang_studi, $request->diploma_tahun_masuk, $request->diploma_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->diploma);
+            $table_pendidikan->jenjang_pendidikan = 'diploma';
+            $table_pendidikan->institusi = $request->diploma_institusi;
+            $table_pendidikan->bidang_studi = $request->diploma_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->diploma_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->diploma_tahun_lulus;
+            $table_pendidikan->save();
+            $pendidikan_id['diploma'] = $table_pendidikan->id;
+        }
+
+        if (isset($request->sarjana_institusi, $request->sarjana_bidang_studi, $request->sarjana_tahun_masuk, $request->sarjana_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->sarjana);
+            $table_pendidikan->jenjang_pendidikan = 'sarjana';
+            $table_pendidikan->institusi = $request->sarjana_institusi;
+            $table_pendidikan->bidang_studi = $request->sarjana_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->sarjana_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->sarjana_tahun_lulus;
+            $table_pendidikan->save();
+            $pendidikan_id['sarjana'] = $table_pendidikan->id;
+        }
+
+        if (isset($request->lainnya_institusi, $request->lainnya_bidang_studi, $request->lainnya_tahun_masuk, $request->lainnya_tahun_lulus)) {
+            $table_pendidikan = pendidikan::findOrCreate($pendidikan_id->lainnya);
+            $table_pendidikan->jenjang_pendidikan = 'lainnya';
+            $table_pendidikan->institusi = $request->lainnya_institusi;
+            $table_pendidikan->bidang_studi = $request->lainnya_bidang_studi;
+            $table_pendidikan->tahun_masuk = $request->lainnya_tahun_masuk;
+            $table_pendidikan->tahun_lulus = $request->lainnya_tahun_lulus;
+            $table_pendidikan->save();
+            $pendidikan_id['lainnya'] = $table_pendidikan->id;
+        }
+
+        $data_utama->pendidikan_id = serialize($pendidikan_id);
+
+        $data_utama->save();
+        $data = $this.getPendaftarFullDetails($data_utama->id);
+        return view('show', compact);
     }
 
     public function verifikasi(Request $request)
@@ -200,5 +318,11 @@ class HomeController extends Controller
         if($data->teman_saudara){return "teman/saudara";}
         if($data->pameran){return "pameran";}
         if($data->lainnya){return "lainnya";}
+    }
+
+    private static function findOrCreate($id)
+    {
+        $obj = static::find($id);
+        return $obj ?: new static;
     }
 }
