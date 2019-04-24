14cd4cbed9e93f197cc90ac5ed627b0f916a03f1
yoshimaputri <yoshimashima@gmail.com>
2019-04-24 23:13:17 +0700

    edit: daftar,edit,homepage,app .blade

--- public/js/sipikti.js
+++ public/js/sipikti.js
@@ -82,9 +82,9 @@ $(".nextMyModal").click(function(){
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
+		'transform': 'scale('+scale+')',
+		'position': 'absolute'
+	  });
 			next_fs.css({'left': left, 'opacity': opacity});
 		}, 
 		duration: 800, 
@@ -98,41 +98,57 @@ $(".nextMyModal").click(function(){
 });
 
 $(".next").click(function(){
-	console.log('next pressed');
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	console.log(next_fs);
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
+	var arr = ['#nama','#nama_gelar','#tempat_lahir','#tanggal_lahir','#nomor_handphone','#asal_jalan','#asal_kelurahan','#asal_kecamatan','#asal_kabupaten','#asal_kode_pos','#asal_telepon','#surabaya_jalan','#surabaya_kelurahan','#surabaya_kecamatan','#surabaya_kabupaten','#surabaya_kode_pos','#surabaya_telepon'];
+	// $('#nama').blur(function(){
+	jQuery.each(arr, function(i,value){
+		if(!$(value).val()) {
+			console.log('belom keisi');
+			// $("#lnama").append("<strong style='color:red;font-size:12px;'>&nbsp;&nbsp;* mohon diisi</strong>");
+			$(value+'_label').show();
+			$('html, body').animate({
+				scrollTop: $(value).offset().top - 100
+			}, 1000, function() {
+				$(value).focus();
+			});
+		}
+		else {
+			console.log('next pressed');
+			if(animating) return false;
+			animating = true;
+			
+			current_fs = $(this).parent();
+			next_fs = $(this).parent().next();
+			console.log(next_fs);
+			//activate next step on progressbar using the index of next_fs
+			$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+			
+			//show the next fieldset
+			next_fs.show(); 
+			//hide the current fieldset with style
+			current_fs.animate({opacity: 0}, {
+				step: function(now, mx) {
+					//as the opacity of current_fs reduces to 0 - stored in "now"
+					//1. scale current_fs down to 80%
+					scale = 1 - (1 - now) * 0.2;
+					//2. bring next_fs from the right(50%)
+					left = (now * 50)+"%";
+					//3. increase opacity of next_fs to 1 as it moves in
+					opacity = 1 - now;
+					current_fs.css({
+				'transform': 'scale('+scale+')',
+				'position': 'absolute'
+			  });
+					next_fs.css({'left': left, 'opacity': opacity});
+				}, 
+				duration: 800, 
+				complete: function(){
+					current_fs.hide();
+					animating = false;
+				}, 
+				//this comes from the custom easing plugin
+				easing: 'easeInOutBack'
+			});
+		}
 	});
 });
 
