b4fb36f59dbc4850e5f4ee7e8e706eada7cfa715
rahandi <rahandinoor@gmail.com>
2019-04-25 10:43:46 +0700

    give data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $data = pendaftar::all();
+        return view('dashboard', compact('data'));
     }
 
     public function list()
