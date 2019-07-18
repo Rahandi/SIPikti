d6f509e55c4ab74d3d122e7e04f17664af78ec13
yoshimaputri <yoshimashima@gmail.com>
2019-07-18 20:49:42 +0700

    rekap done

--- app/Exports/RekapExport.php
+++ app/Exports/RekapExport.php
@@ -18,19 +18,43 @@ class RekapExport implements FromCollection, WithHeadings
         ->join('angsuran', 'mahasiswa_angsuran.angsuran_id', '=', 'angsuran.id')
         ->select('mahasiswa.nrp', 'mahasiswa.nama', 'angsuran.nama as angsuran_nama', 'mahasiswa_angsuran.data_pembayaran')
         ->get();
+        $data_array = array();
         for($i=0;$i<count($data);$i++){
             $data[$i]->data_pembayaran = unserialize($data[$i]->data_pembayaran);
             // disintegrate pembayaran
-            $data_pembayaran = array(
-                "Daftar ulang 1" => $data[$i]->data_pembayaran['Daftar ulang 1']['tanda'],
-                "Daftar ulang 2" => $data[$i]->data_pembayaran['Daftar ulang 2']['tanda']
+            $temp_data = array(
+                "nrp" => $data[$i]->nrp,
+                "nama" => $data[$i]->nama,
+                "angsuran_nama" => $data[$i]->angsuran_nama
             );
+            array_push($data_array, $temp_data);
+            $data_array[$i]["Daftar ulang 1"] = ($data[$i]->data_pembayaran['Daftar ulang 1']['tanda'] == 1) ? "sudah bayar" : "belum bayar";
+            $data_array[$i]["Daftar ulang 2"] = ($data[$i]->data_pembayaran['Daftar ulang 2']['tanda'] == 1) ? "sudah bayar" : "belum bayar";
             for($j=1;$j<=5;$j++){
                 $angsuran = "Angsuran ".$j;
-                $data_pembayaran[$angsuran] = isset($data[$i]->data_pembayaran[$angsuran]) ? $data[$i]->data_pembayaran[$angsuran]['tanda'] : -1;
+                $data_array[$i][$angsuran] = isset($data[$i]->data_pembayaran[$angsuran]) ? $data[$i]->data_pembayaran[$angsuran]['tanda'] : -1;
+                $data_array[$i][$angsuran] = ($data_array[$i][$angsuran] == 1) ? "sudah bayar" : $data_array[$i][$angsuran];
+                $data_array[$i][$angsuran] = ($data_array[$i][$angsuran] == 0) ? "belum bayar" : $data_array[$i][$angsuran];
+                $data_array[$i][$angsuran] = ($data_array[$i][$angsuran] == -1) ? "-" : $data_array[$i][$angsuran];
             }
-            $data[$i]->data_pembayaran = $data_pembayaran;
         }
-        
+
+        return collect($data_array);
+    }
+
+    public function headings(): array
+    {
+        return [
+            "nrp",
+            "nama",
+            "angsuran_nama",
+            "Daftar ulang 1",
+            "Daftar ulang 2",
+            "Angsuran 1",
+            "Angsuran 2",
+            "Angsuran 3",
+            "Angsuran 4",
+            "Angsuran 5",
+        ];
     }
 }
--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -118,25 +118,7 @@ class PembayaranController extends Controller
 
     public function download()
     {
-        $data = \DB::table('mahasiswa_angsuran')
-        ->join('mahasiswa', 'mahasiswa_angsuran.mahasiswa_id', '=', 'mahasiswa.id')
-        ->join('angsuran', 'mahasiswa_angsuran.angsuran_id', '=', 'angsuran.id')
-        ->select('mahasiswa.nrp', 'mahasiswa.nama', 'angsuran.nama as angsuran_nama', 'mahasiswa_angsuran.data_pembayaran')
-        ->get();
-        for($i=0;$i<count($data);$i++){
-            $data[$i]->data_pembayaran = unserialize($data[$i]->data_pembayaran);
-            // disintegrate pembayaran
-            $data_pembayaran = array(
-                "Daftar ulang 1" => $data[$i]->data_pembayaran['Daftar ulang 1']['tanda'],
-                "Daftar ulang 2" => $data[$i]->data_pembayaran['Daftar ulang 2']['tanda']
-            );
-            for($j=1;$j<=5;$j++){
-                $angsuran = "Angsuran ".$j;
-                $data_pembayaran[$angsuran] = isset($data[$i]->data_pembayaran[$angsuran]) ? $data[$i]->data_pembayaran[$angsuran]['tanda'] : -1;
-            }
-            $data[$i]->data_pembayaran = $data_pembayaran;
-        }
-        dd($data);
+        return Excel::download(new RekapExport(), 'rekap.xlsx');
     }
 
     public function generateNRP($id_mahasiswa)
