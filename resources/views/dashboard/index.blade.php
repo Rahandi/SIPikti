1dc980e05928689276141ea8dea3562c7e518351
yoshimaputri <yoshimashima@gmail.com>
2019-08-14 15:52:44 +0700

    statistik total pendaftar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,12 +14,12 @@
 								<li>
 									<span class="circle circle-md bg-danger"><i class="ti-clipboard"></i></span>
 								</li>
-								<li class="col-last"><h3 class="counter text-right m-t-15">23</h3></li>
+								<li class="col-last"><h3 class="counter text-right m-t-15">{{$data['pendaftar']}}</h3></li>
 								<li class="col-middle">
 									<h4>Total Pendaftar</h4>
 									<div class="progress">
-									  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%"> 
-										  <span class="sr-only">40% Complete (success)</span> 
+									  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%"> 
+										  <span class="sr-only">80% Complete (success)</span> 
 									  </div>
 									</div>
 								</li>
