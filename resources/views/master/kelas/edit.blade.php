b58edc4332b513fc4ae615de2338f0a5d0d642a1
yoshimaputri <yoshimashima@gmail.com>
2019-08-30 21:36:45 +0700

    edit kelas

--- resources/views/master/kelas/edit.blade.php
+++ resources/views/master/kelas/edit.blade.php
@@ -29,34 +29,35 @@
 					<tr>
 						<td style="border-right: 1px solid black;">
 							<div style="vertical-align: middle;" class="form-group">
-								<input style="text-align: center;" type="text" class="form-control" name="nama" placeholder="A" required>
+								<input style="text-align: center;" type="text" class="form-control" name="nama" placeholder="A" value="{{$data->nama}}" required>
 							</div>
 						</td>
 						<td>
 							<div class="form-group" style="width: 100%;">
-								<input type="time" class="form-control" name="start_SK" required>
+								<input type="time" class="form-control" name="start_SK" value="{{$data->start_SK}}" required>
 							</div>
 						</td>
 						<td style="vertical-align: top;">s/d</td>
 						<td style="border-right: 1px solid black;">
 							<div class="form-group">
-								<input type="time" class="form-control" name="end_SK" required>
+								<input type="time" class="form-control" name="end_SK" value="{{$data->end_SK}}" required>
 							</div>
 						</td>
 						<td>
 							<div class="form-group">
-								<input type="time" class="form-control" name="start_J" required>
+								<input type="time" class="form-control" name="start_J" value="{{$data->start_J}}" required>
 							</div>
 						</td>
 						<td style="vertical-align: top;">s/d</td>
 						<td>
 							<div class="form-group">
-								<input type="time" class="form-control" name="end_J" required>
+								<input type="time" class="form-control" name="end_J" value="{{$data->end_J}}" required>
 							</div>
 						</td>
 					</tr>
 				</table>
 				<div style="text-align: center; width: 100%;">
+					<input type="hidden" name="id" value="{{$data->id}}">
 					<a><button type="submit" style="width: 15%;margin-top: 2%;" class="btn btn-info">Submit</button></a>
 				</div>
 			</form>
