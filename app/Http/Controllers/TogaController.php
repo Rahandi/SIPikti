35aa7ca656421a2841e15e28fa146efba70725f5
rahandi <rahandinoor@gmail.com>
2019-11-30 16:47:26 +0700

    data toga

--- app/Http/Controllers/TogaController.php
+++ app/Http/Controllers/TogaController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\mahasiswa;
+use App\mahasiswaJadwal;
+use App\jadwal;
+use App\masterKelas;
 
 class TogaController extends Controller
 {
@@ -22,6 +25,13 @@ class TogaController extends Controller
     public function kwitansi(Request $request)
     {
         $mahasiswa = mahasiswa::find($request->id);
+        $mahasiswaJadwal = mahasiswaJadwal::where('mahasiswa_id', $request->id)->first();
+        $jadwal = jadwal::find($mahasiswaJadwal->jadwal_id);
+        $kelas = masterKelas::find($jadwal->id_kelas);
+
+        $data = new \strClass();
+        $data->mahasiswa = $mahasiswa->nama;
+        $data->kelas = $kelas->nama;
         return view ('keuangan.toga.kwitansi', compact('data'));
     }
 }
